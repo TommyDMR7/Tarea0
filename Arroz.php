563c4ac77c84ee135fc7538c59fbb7b996a7da75
TommyDMR7 <159972335+TommyDMR7@users.noreply.github.com>
2024-02-16 00:47:37 -0600

    Add files via upload

---
+++ Arroz.php
@@ -0,0 +1,51 @@
+<html>
+<body>
+<h1>12618. Practicando con formulas </h1>
+
+<h2>Descripción</h2>
+<h4>Dado un valor real quieres resolver una serie de ecuaciones hasta obtener el valor de z.</h4>
+
+<table border="1">
+  <tr>
+    <td>
+      <?php
+        $w = "3a + 15";
+        $a = "x + 3/(x + 1)";
+        $s = "(5x + 7y)/(a*x*y)";
+        
+        echo "<h4>x = $w</h4>";
+        echo "<h4>y = $a</h4>";
+        echo "<h4>z = $s</h4>";
+      ?>
+    </td>
+  </tr>
+  </table>
+
+
+<h4>Un real a. Puedes suponer que 1 < a < 100.</h4>
+<h2>Salida</h2>
+<h4>Un real que sea el valor de z
+ impreso con tres puntos decimales.</h4>
+ </table>
+    <table border="1" cellspacing="3"cellpading="3" class="reservas">
+    <tr>
+ 
+
+<th>Entrada</th><th> Salida</th>
+</td>
+</tr>
+<tr>
+<h3>Ejemplo </h3>
+
+<td>33.562</td><td>  0.148</td>
+
+<tr>
+<td>72.569</td><td> 0.069</td>
+</tr>
+<td>64.835</td><td> 0.077</td>
+
+
+</tr>
+</table>
+</body>
+<html>
